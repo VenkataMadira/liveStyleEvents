a21a252c3d3213c4e12a281f61c8e09c97b2175e
VenkataMadira <madira1587@gmail.com>
2019-10-05 17:52:13 +0100

    first commit

---
+++ LiveStyleEvents/Event.swift
@@ -0,0 +1,25 @@
+//
+//  Event.swift
+//  LiveStyleEvents
+//
+//  Created by Venkat Madira on 05/10/2019.
+//  Copyright © 2019 Venkat Madira. All rights reserved.
+//
+
+import UIKit
+
+class Event: Codable {
+    let id: String
+    let title: String
+    let image: String
+    let startDate: Int64
+    
+    init( id : String,title : String,image : String,startDate : Int64,hasFav:Bool) {
+        self.id = id
+        self.title = title
+        self.image  = image
+        self.startDate = startDate
+        
+        
+    }
+}
---
+++ LiveStyleEvents/EventTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventTableViewCell.swift
+//  LiveStyleEvents
+//
+//  Created by Venkat Madira on 05/10/2019.
+//  Copyright © 2019 Venkat Madira. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
